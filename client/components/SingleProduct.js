fcb6e740eecb2f2e98ff2ed7dfcddda8f9e8ef93
Kasia Gierat <kasia.gierat@gmail.com>
2021-01-13 17:32:29 -0500

    Single product FE

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchSingleProduct} from '../store/singleProduct'
+import {Button} from '@material-ui/core'
 
 class SingleProduct extends React.Component {
   componentDidMount() {
-    console.log(this.props.match.params.productId)
     this.props.loadSingleProduct(this.props.match.params.productId)
   }
 
@@ -14,7 +14,34 @@ class SingleProduct extends React.Component {
     return (
       <div>
         {product.title ? (
-          <div>{product.title}</div>
+          <div className="singleProductContainer">
+            <div>
+              <img src={product.imageUrl} className="book-img" />
+            </div>
+            <div>
+              <div>Title: {product.title}</div>
+              <div>Author: {product.author}</div>
+              <div>Description: {product.description}</div>
+              <div>Price: ${product.price / 100}</div>
+            </div>
+
+            <label hmtlFor="Quantity">Quantity</label>
+            <select name="Quantity" value="1">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <Button
+              variant="contained"
+              size="small"
+              type="button"
+              color="primary"
+            >
+              Add to cart
+            </Button>
+          </div>
         ) : (
           <span>Book not found</span>
         )}
