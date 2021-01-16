32cd5a2661ce4a9f8e8045a52a76b4737db039a9
Kasia Gierat <kasia.gierat@gmail.com>
2021-01-16 15:06:29 -0500

    image url to seed data

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -34,8 +34,11 @@ const Product = db.define('product', {
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue:
-      'https://media1.thehungryjpeg.com/thumbs2/ori_3483795_dfe015d7ef50179be53dc30114c6bf7ddd4c57c3_white-blank-book-cover-vector-template.jpg'
+      // 'https://media1.thehungryjpeg.com/thumbs2/ori_3483795_dfe015d7ef50179be53dc30114c6bf7ddd4c57c3_white-blank-book-cover-vector-template.jpg'
+      // 'https://tajfuny.pl/wp-content/uploads/2020/10/concrete-tokyo-map.jpg',
+      'https://images.unsplash.com/photo-1461419912973-9964f1f54b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80'
   },
+
   genre: {
     type: Sequelize.ENUM(
       'Romance',
