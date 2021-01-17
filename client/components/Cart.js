2764798986576ee280d8afce947f5e05185249f2
Janet Lam <janetlam3933@gmail.com>
2021-01-16 20:50:37 -0500

    disabled checkout button if cart is empty

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -29,60 +29,74 @@ class Cart extends React.Component {
       <div className="cart-container">
         <h1>Your Shopping Cart</h1>
         {!products.length ? (
-          <h3>Your Cart is Empty</h3>
+          <div>
+            <h3>Your Cart is Empty</h3>
+            <Button
+              type="button"
+              variant="contained"
+              size="small"
+              color="primary"
+              className="button"
+              disabled={true}
+            >
+              Go to Checkout
+            </Button>
+          </div>
         ) : (
-          products.map(product => (
-            <div key={product.id} className="cartItemContainer">
-              <div>
-                <Link to={`/products/${product.id}`}>
-                  <img src={product.imageUrl} className="book-img" />
-                </Link>
+          <div>
+            {products.map(product => (
+              <div key={product.id} className="cartItemContainer">
+                <div>
+                  <Link to={`/products/${product.id}`}>
+                    <img src={product.imageUrl} className="book-img" />
+                  </Link>
 
-                <p>Price: ${product.price / 100}</p>
-                <p>Item Subtotal - Add Actual Amount Here</p>
+                  <p>Price: ${product.price / 100}</p>
+                  <p>Item Subtotal - Add Actual Amount Here</p>
+                </div>
+
+                <p>
+                  {product.title} | {product.author}
+                </p>
+
+                <div>
+                  <label htmlFor="quantity">Quantity</label>
+                  <select
+                    name="quantity"
+                    value={this.state.quantity}
+                    onChange={this.handleChange}
+                  >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <Button
+                    type="button"
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    className="button"
+                  >
+                    Delete Item
+                  </Button>
+                </div>
               </div>
-
-              <p>
-                {product.title} | {product.author}
-              </p>
-
-              <div>
-                <label htmlFor="quantity">Quantity</label>
-                <select
-                  name="quantity"
-                  value={this.state.quantity}
-                  onChange={this.handleChange}
-                >
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </select>
-                <Button
-                  type="button"
-                  variant="contained"
-                  size="small"
-                  color="primary"
-                  className="button"
-                >
-                  Delete Item
-                </Button>
-              </div>
-            </div>
-          ))
+            ))}
+            <Link to="/checkout">
+              <Button
+                type="button"
+                variant="contained"
+                size="small"
+                color="primary"
+                className="button"
+              >
+                Go to Checkout
+              </Button>
+            </Link>
+          </div>
         )}
-        <Link to="/checkout">
-          <Button
-            type="button"
-            variant="contained"
-            size="small"
-            color="primary"
-            className="button"
-          >
-            Go to Checkout
-          </Button>
-        </Link>
       </div>
     )
   }
