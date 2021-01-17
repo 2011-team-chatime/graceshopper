54534cd4ffa1dc261876c5f8cacef4c12ecdbbb8
Kasia Gierat <kasia.gierat@gmail.com>
2021-01-16 23:42:52 -0500

    add cart items

---
+++ client/components/CartIcon.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import {fetchCart} from '../store/cart'
+import {render} from 'enzyme'
+
+class CartIcon extends React.Component {
+  componentDidMount() {
+    this.props.fetchCart()
+  }
+  render() {
+    const {products} = this.props.cart
+    console.log('products', products)
+    return (
+      <div>
+        <ShoppingCartIcon fontSize="large" />
+        <div className="itemsInCart">
+          <div>{products ? products.length : 0}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCart: () => dispatch(fetchCart())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
