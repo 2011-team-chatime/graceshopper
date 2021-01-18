055390d84d49e833f7977cabb20fd6f5b77cbcf1
Kasia Gierat <kasia.gierat@gmail.com>
2021-01-18 01:08:55 -0500

    test for not admin

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -72,6 +72,9 @@ describe('Product routes', () => {
         password: 'sAEwCzN'
       })
 
+      // agent is the 'instance' of express app which calls route to login the user
+      // the state of login user will be stored in agent instance
+      // and it will be used to call every other routes in following tests
       agent = request.agent(app)
       await agent
         .post('/auth/login')
@@ -148,6 +151,92 @@ describe('Product routes', () => {
         .delete('/api/products/1')
         .set('Content-type', 'application/json')
         .expect(200)
+    })
+  })
+
+  describe('Testing adding, editing and deleting products in db by not admin user', () => {
+    let agent
+    beforeEach(async () => {
+      await User.create({
+        name: 'Donna',
+        isAdmin: false,
+        address: '74 Bunting Trail',
+        paymentinfo: '3579187745757906',
+        email: 'dmcgroarty0@jiathis.com',
+        password: 'sAEwCzN'
+      })
+
+      agent = request.agent(app)
+      await agent
+        .post('/auth/login')
+        .set('Content-type', 'application/json')
+        .send(
+          JSON.stringify({
+            email: 'dmcgroarty0@jiathis.com',
+            password: 'sAEwCzN'
+          })
+        )
+        .expect(200)
+    })
+
+    it('POST /api/products', async () => {
+      const res = await agent
+        .post('/api/products')
+        .set('Content-type', 'application/json')
+        .send(
+          JSON.stringify({
+            title: 'Learn How to Code',
+            author: 'Fullstack Academy',
+            price: 50,
+            quantity: 40,
+            genre: 'Sci-fi',
+            description:
+              'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.'
+          })
+        )
+        .expect(401)
+    })
+
+    it('PUT /api/products/:productId', async () => {
+      await Product.create({
+        title:
+          'nulla integer pede justo lacinia eget tincidunt eget tempus vel',
+        author: 'Davita Bleasdale',
+        price: 70,
+        quantity: 58,
+        genre: 'Other',
+        description:
+          'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.'
+      })
+
+      const res = await agent
+        .put('/api/products/1')
+        .set('Content-type', 'application/json')
+        .send(
+          JSON.stringify({
+            title: 'Learn How to Code',
+            author: 'Fullstack Academy'
+          })
+        )
+        .expect(401)
+    })
+
+    it('DELETE /api/products/:productId', async () => {
+      await Product.create({
+        title:
+          'nulla integer pede justo lacinia eget tincidunt eget tempus vel',
+        author: 'Davita Bleasdale',
+        price: 70,
+        quantity: 58,
+        genre: 'Other',
+        description:
+          'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.'
+      })
+
+      await agent
+        .delete('/api/products/1')
+        .set('Content-type', 'application/json')
+        .expect(401)
     })
   })
 })
