cb7e685c2c491f4462e18b0d20801a7e0831814a
Kasia Gierat <kasia.gierat@gmail.com>
2021-01-18 00:47:27 -0500

    product routes for admin and test spec

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
+const {isAdmin} = require('./util')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -21,5 +22,41 @@ router.get('/:productId', async (req, res, next) => {
     }
   } catch (error) {
     next(error)
+  }
+})
+
+//// For Admin Only ////
+// to update product by product id
+router.put('/:productId', isAdmin, async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    await product.update(req.body)
+    res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// to add single product
+router.post('/', isAdmin, async (req, res, next) => {
+  try {
+    const newProduct = await Product.create(req.body)
+    res.status(200).json(newProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// to delete the product by product id
+router.delete('/:productId', isAdmin, async (req, res, next) => {
+  try {
+    await Product.destroy({
+      where: {
+        id: req.params.productId
+      }
+    })
+    res.status(200).send('Product removed')
+  } catch (err) {
+    next(err)
   }
 })
