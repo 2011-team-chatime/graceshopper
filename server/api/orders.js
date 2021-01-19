c55aa55f393dcd4ac0834768af578218402dfec1
Comfort Olowo <c.olowo12@gmail.com>
2021-01-18 19:14:30 -0500

    Fixed Total Rendering Issue

--- server/api/orders.js
+++ server/api/orders.js
@@ -45,9 +45,10 @@ router.post('/add/:productId', async (req, res, next) => {
       } else {
         await updatedOrder.addProduct(product)
       }
+
+      await updatedOrder.reload()
       await updatedOrder.updateTotal()
       await updatedOrder.save()
-      await updatedOrder.reload()
 
       res.json(updatedOrder)
     } else {
@@ -67,9 +68,10 @@ router.put('/remove/:productId', async (req, res, next) => {
       })
       const product = await Product.findByPk(req.params.productId)
       await order.removeProduct(product)
-      await order.updateTotal()
-      await order.save()
       await order.reload()
+      await order.updateTotal()
+
+      await order.save()
 
       res.json(order)
     } else {
@@ -95,7 +97,6 @@ router.put('/:orderId/checkout', async (req, res, next) => {
   }
 })
 
-
 router.post('/:userId', async (req, res, next) => {
   try {
     let order = await Order.findOne({
@@ -109,16 +110,13 @@ router.post('/:userId', async (req, res, next) => {
         userId: req.params.userId
       })
     }
-    // else {
-    //   await order.update(req.body)
-    // }
+
     await order.reload()
     res.json(order)
   } catch (err) {
     next(err)
-    }
+  }
 })
-
 
 router.put('/sub/:productId', async (req, res, next) => {
   try {
@@ -128,24 +126,22 @@ router.put('/sub/:productId', async (req, res, next) => {
         where: {userId: req.user.id, status: 'inCart'}
       })
       const product = await Product.findByPk(req.params.productId)
-      const hasProduct = await updatedOrder.hasProduct(product)
-      if (hasProduct) {
-        const item = await Item.findOne({
-          where: {orderId: updatedOrder.id, productId: product.id}
-        })
-        await item.update({cartQuantity: (item.cartQuantity -= 1)})
-      } else {
-        await updatedOrder.removeProduct(product)
-      }
+
+      const item = await Item.findOne({
+        where: {orderId: updatedOrder.id, productId: product.id}
+      })
+      await item.update({cartQuantity: (item.cartQuantity -= 1)})
+
+      await updatedOrder.reload()
+
       await updatedOrder.updateTotal()
       await updatedOrder.save()
-      await updatedOrder.reload()
+
       res.json(updatedOrder)
     } else {
       res.send({})
     }
   } catch (error) {
     next(error)
-
   }
 })
