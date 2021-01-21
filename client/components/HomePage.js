2637c4376609326fc96e95bafb78987492cb40cb
Comfort Olowo <c.olowo12@gmail.com>
2021-01-20 19:13:58 -0500

    second commit

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchCart} from '../store/cart'
 
-const Home = () => {
-  return (
-    <div className="homeImg">
-      <img src="https://images.unsplash.com/photo-1472068996216-8c972a0af9bd?ixid=MXwxMjA3fDB8MHxwaG90[…]ufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1654&q=80" />
-    </div>
-  )
+class Home extends React.Component {
+  componentDidMount() {
+    this.props.fetchCart()
+  }
+  render() {
+    return (
+      <div className="homeImg">
+        <img src="https://images.unsplash.com/photo-1472068996216-8c972a0af9bd?ixid=MXwxMjA3fDB8MHxwaG90[…]ufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1654&q=80" />
+      </div>
+    )
+  }
 }
 
-export default Home
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCart: () => dispatch(fetchCart())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
